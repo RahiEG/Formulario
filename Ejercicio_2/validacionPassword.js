351e5a9d46f22727e595e815e1050a1fa516f59c
Rahi Escamilla <rahiloag@gmail.com>
2026-04-07 11:02:04 -0600

    Poniendo limite de tiempo en las alertas

--- Ejercicio_2/validacionPassword.js
+++ Ejercicio_2/validacionPassword.js
@@ -7,7 +7,22 @@ formulario.addEventListener("submit", function(event) {
 
         if (nombre !== "" && password.length >= 8) {
             formulario.style.backgroundColor = "green";
+            setTimeout(()=>{
+                formulario.style.background="none"
+                formulario.style.transition="2s"
+            },5000);
+            formulario.elements["nombre"].value=""
+            formulario.elements["password"].value=""
+            formulario.elements["nombre"].focus();
         } else {
+            
             formulario.style.backgroundColor = "red";
+             setTimeout(()=>{
+                formulario.style.background="none"
+                formulario.style.transition="2s"
+            },5000);
+            formulario.elements["nombre"].focus();
+
         }
-    });
+    })
+
