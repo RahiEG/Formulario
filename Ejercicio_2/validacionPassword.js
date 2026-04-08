c55f6e33b2caab3554814c0c42d400a85e452152
Rahi Escamilla <rahiloag@gmail.com>
2026-04-08 10:28:15 -0600

    Estilos de mensajes

--- Ejercicio_2/validacionPassword.js
+++ Ejercicio_2/validacionPassword.js
@@ -6,21 +6,25 @@ formulario.addEventListener("submit", function(event) {
         let password = document.getElementById("password").value;
 
         if (nombre !== "" && password.length >= 8) {
-            formulario.style.backgroundColor = "green";
+            formulario.style.backgroundColor = "#d4edda";
+            document.getElementById("mensaje-ok").textContent="¡Formulario enviado correctamente!"
             setTimeout(()=>{
                 formulario.style.background="none"
                 formulario.style.transition="2s"
-            },5000);
+                 document.getElementById("mensaje-ok").textContent=""
+            },2000);
             formulario.elements["nombre"].value=""
             formulario.elements["password"].value=""
             formulario.elements["nombre"].focus();
         } else {
             
-            formulario.style.backgroundColor = "red";
+            formulario.style.backgroundColor = "#f8d7da";
+             document.getElementById("mensaje-error").textContent="La contraseña debe contener al menos 8 caracteres"
              setTimeout(()=>{
                 formulario.style.background="none"
                 formulario.style.transition="2s"
-            },5000);
+                document.getElementById("mensaje-error").textContent=""
+            },2000);
             formulario.elements["nombre"].focus();
 
         }
